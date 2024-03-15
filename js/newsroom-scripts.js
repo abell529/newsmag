6e22f1ad0752fe463a2d85eadb44f2832132e73f
abell <abell@towson.edu>
2024-03-15 14:39:56 -0400

    slide changes

--- js/newsroom-scripts.js
+++ js/newsroom-scripts.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
         if (isVisible) {
             content.animate({ left: '-550px' }, 'fast').data('visible', false);
         } else {
-            content.animate({ left: '0px' }, 'fast').data('visible', true); /* 40px is the height of the header */
+            content.animate({ left: '0px' }, 'fast').data('visible', true);
         }
     });
 
